0a5feee65a51ec3eba9fda73951bdba4b41eca72
RobertsG9751 <sapins23@gmail.com>
2023-02-08 19:17:13 +0200

    Papildints steam login. Lietotajam tiek izvaditi vina dati.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,50 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [isUser, setIsUser] = useState(false)
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  console.log(localStorage.getItem("user_id"))
+  useEffect(()=>{
+    if(urlParams.get('steamid')){
+      localStorage.setItem("user_id", urlParams.get('steamid'))
+      localStorage.setItem("user_name", urlParams.get('username'))
+      localStorage.setItem("user_avatar", urlParams.get('avatar'))
+      setIsUser(true)
+    }
+  }, [])
+  useEffect(()=>{
+    if(localStorage.getItem("user_id"))
+    setIsUser(true)
+  }, [])
+
+  const logout = () => {
+    localStorage.removeItem("user_id")
+    localStorage.removeItem("user_name")
+    localStorage.removeItem("user_avatar")
+    console.log("logged out")
+  }
+
+  ///// TEST UZMETUMS
+  ///// Parāda lietotājam steam_id, username un avatar pēc steam login
   return (
     <div className="App">
-      <a href="http://localhost:3001/api/auth/steam">Sign in</a>
+      {
+        isUser===false &&(
+          <a href="http://localhost:3001/api/auth/steam">Sign in</a>
+        )
+      }{
+        isUser===true &&(
+          <>
+            <p>Steam_id: {localStorage.getItem("user_id")}</p>
+            <p>Username: {localStorage.getItem("user_name")}</p>
+            <img src={localStorage.getItem("user_avatar")}></img>
+            <br></br>
+            <a onClick={logout} href='http://localhost:3000/'>Logout</a>
+          </>
+        )
+      }
     </div>
   );
 }
