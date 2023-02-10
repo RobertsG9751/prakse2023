783e167589b7b6fb9a44c6327755563a10b970c6
ralfsiraks <hyperblazexd@gmail.com>
2023-02-10 18:18:31 +0200

    cita login poga

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,14 +1,18 @@
-import style from "./Login.module.css"
-import React from "react"
+import style from "./Login.module.css";
+import React from "react";
 
-const Login = () =>{
-    return(
-        <div className={`center_items ${style.wrapper}`}>
-            <h2 className={style.heading}>Sign in with your steam account!</h2>
-            <a href="http://localhost:3001/api/auth/steam">
-                <img className={style.image} alt="#" src="http://wargaming.army/wp-content/uploads/2018/10/loginsteamlarge.png"></img>
-            </a>
-        </div>
-    )
-}
-export default Login
+const Login = () => {
+  return (
+    <div className={`center_items ${style.wrapper}`}>
+      <h2 className={style.heading}>Sign in with your steam account!</h2>
+      <a href="http://localhost:3001/api/auth/steam">
+        <img
+          className={style.image}
+          alt="#"
+          src="https://cdn.discordapp.com/attachments/707559271793164292/1073637512880336918/steamlogin.png"
+        ></img>
+      </a>
+    </div>
+  );
+};
+export default Login;
