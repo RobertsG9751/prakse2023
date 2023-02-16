be69f06a9cf761e7d7f217190f0e519a37ce8bd5
ralfsiraks <hyperblazexd@gmail.com>
2023-02-16 02:59:53 +0200

    main banner done | izlabota login poga

--- frontend/src/Components/Main.js
+++ frontend/src/Components/Main.js
@@ -8,10 +8,16 @@ const Main = () => {
     <main className={style.main}>
       <div className={style.banner}>
         <img
-          className={style.img}
-          src="https://cdn.discordapp.com/attachments/707559271793164292/1075556978719457301/counter-strike-global-offensive-4k-new-af-2560x1440_1.jpg"
+          className={style.banner_img}
+          src="https://cdn.discordapp.com/attachments/707559271793164292/1075562550692950046/counter-strike-global-offensive-4k-new-af-2560x1440_1.jpg"
         ></img>
         <h1 className={style.h1}>TIME TO MAKE SOME BREAD</h1>
+      </div>
+      <div className={style.crypto_sec}>
+        <img
+          className={style.crypto_img}
+          src="https://cdn.discordapp.com/attachments/707559271793164292/1075572080109703268/coin_1.jpg"
+        ></img>
       </div>
     </main>
   );
