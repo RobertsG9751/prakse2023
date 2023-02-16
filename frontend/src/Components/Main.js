0ab06fb65ff4cf0175609b4af91f61da92e250a8
ralfsiraks <hyperblazexd@gmail.com>
2023-02-16 22:22:43 +0200

    main daļas turpinājums. crypto sekcija

--- frontend/src/Components/Main.js
+++ frontend/src/Components/Main.js
@@ -11,13 +11,24 @@ const Main = () => {
           className={style.banner_img}
           src="https://cdn.discordapp.com/attachments/707559271793164292/1075562550692950046/counter-strike-global-offensive-4k-new-af-2560x1440_1.jpg"
         ></img>
-        <h1 className={style.h1}>TIME TO MAKE SOME BREAD</h1>
+        <h1 className={style.banner_text}>TIME TO MAKE SOME BREAD</h1>
       </div>
       <div className={style.crypto_sec}>
         <img
           className={style.crypto_img}
-          src="https://cdn.discordapp.com/attachments/707559271793164292/1075572080109703268/coin_1.jpg"
+          src="https://cdn.discordapp.com/attachments/707559271793164292/1075833695736434840/coin.jpg"
         ></img>
+        <div className={style.crypto_wrapper}>
+          <h1 className={style.crypto_text}>
+            Get paid securely in cryptocurrency
+          </h1>
+          <a
+            href="http://localhost:3001/api/auth/steam"
+            className={style.crypto_btn}
+          >
+            <span>Read more</span>
+          </a>
+        </div>
       </div>
     </main>
   );
